8a9f972b334efac348ca86a5018b64e46cc45c98
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2016-03-28 11:46:31 +0200

    Home screen
    Event handling

--- app/src/main/java/cz/hlubyluk/myapplication/MainActivity.java
+++ app/src/main/java/cz/hlubyluk/myapplication/MainActivity.java
@@ -1,13 +1,45 @@
 package cz.hlubyluk.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ClickHandler clickHandler = new ClickHandler(this);
+
+        Button create = (Button) findViewById(R.id.home_create);
+        if (create != null) {
+            create.setOnClickListener(clickHandler);
+        }
+
+        Button list = (Button) findViewById(R.id.home_list);
+        if (list != null) {
+            list.setOnClickListener(clickHandler);
+        }
+    }
+
+    private class ClickHandler implements View.OnClickListener {
+
+        private final Activity activity;
+
+        public ClickHandler(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "onClick() called with: " + "v.getId = [" + v.getId() + "]");
+
+        }
     }
 }
