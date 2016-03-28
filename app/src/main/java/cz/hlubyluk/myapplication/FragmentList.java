4fdb38ea70940741ae5cae391b447a399d157488
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2016-03-28 18:35:23 +0200

    Base View

--- app/src/main/java/cz/hlubyluk/myapplication/FragmentList.java
+++ app/src/main/java/cz/hlubyluk/myapplication/FragmentList.java
@@ -1,11 +1,14 @@
 package cz.hlubyluk.myapplication;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by HlubyLuk on 28.03.16.
@@ -15,6 +18,23 @@ public class FragmentList extends Fragment{
 
     public static Fragment newInstance() {
         return new FragmentList();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        setUp(context);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        setUp(activity);
+    }
+
+    private void setUp(Context context) {
     }
 
     @Nullable
@@ -28,5 +48,7 @@ public class FragmentList extends Fragment{
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ListView list = (ListView) view.findViewById(R.id.list_list);
     }
 }
