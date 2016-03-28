ee1935a4b7dc926e9320ed9d1c37cd2c18063b56
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2016-03-28 18:28:45 +0200

    Read from db

--- app/src/main/java/cz/hlubyluk/myapplication/database/Database.java
+++ app/src/main/java/cz/hlubyluk/myapplication/database/Database.java
@@ -3,12 +3,14 @@ package cz.hlubyluk.myapplication.database;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.text.Editable;
-import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.hlubyluk.myapplication.entity.DBEvent;
 import cz.hlubyluk.myapplication.entity.Kind;
 import cz.hlubyluk.myapplication.entity.Place;
 
@@ -26,21 +28,18 @@ public class Database extends SQLiteOpenHelper {
     private static final String KIND_ID = "kind_id";
     private static final String DATE = "date_event";
 
-//    private static final String DATABASE_CREATE = "create table "
-//            + TABLE + "(" +
-//            "id integer primary key autoincrement, " +
-//            KIND_ID + " text not null);";
-
+    private static final String[] COLUMNS = {KIND, FROM, TO, KIND_ID, DATE};
     private static final String DATABASE_CREATE =
             String.format(
-                    "CREATE TABLE %s (id INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER, %s TEXT NOT NULL);",
+                    "CREATE TABLE %s (id INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s INTEGER, %s INTEGER, %s TEXT NOT NULL, %s INTEGER);",
                     TABLE,
                     KIND,
                     FROM,
                     TO,
-                    DATE,
-                    KIND_ID
+                    KIND_ID,
+                    DATE
             );
+
     private static final String DATABASE_DROP =
             String.format(
                     "DROP TABLE IF EXISTS %s ;",
@@ -74,4 +73,28 @@ public class Database extends SQLiteOpenHelper {
         database.insert(TABLE, null, values);
         database.close();
     }
+
+    public List<DBEvent> getAll() {
+        SQLiteDatabase database = this.getReadableDatabase();
+
+        Cursor cursor = database.query(TABLE, COLUMNS, null, null, null, null, null);
+        cursor.moveToFirst();
+
+        ArrayList<DBEvent> ret = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            int kind = cursor.getInt(0);
+            int from = cursor.getInt(1);
+            int to = cursor.getInt(2);
+            String id = cursor.getString(3);
+            long timeStamp = cursor.getLong(4);
+
+            DBEvent dbEvent = new DBEvent(kind, from, to, id, timeStamp);
+            ret.add(dbEvent);
+        }
+
+        cursor.close();
+
+        return ret;
+    }
+
 }
--- app/src/main/java/cz/hlubyluk/myapplication/entity/Kind.java
+++ app/src/main/java/cz/hlubyluk/myapplication/entity/Kind.java
@@ -1,5 +1,7 @@
 package cz.hlubyluk.myapplication.entity;
 
+import android.support.annotation.Nullable;
+
 /**
  * Created by HlubyLuk on 28.03.16.
  */
@@ -31,5 +33,17 @@ public enum Kind implements Item {
     @Override
     public String getName() {
         return name;
+    }
+
+    @Nullable
+    public static Kind find(int id) {
+        Kind[] values = values();
+        for (Kind value : values) {
+            if (value.getId() == id){
+                return value;
+            }
+        }
+
+        return null;
     }
 }
